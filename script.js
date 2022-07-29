0f2eede9603c54284fb2e89505078c6969b621b5
Nicolas Tortora <nico.t@live.com.ar>
2022-07-29 19:54:04 -0300

    modificacion-funciones

--- script.js
+++ script.js
@@ -1,31 +1,35 @@
 // Crear una clase ejecutable con 3 objetos de tipo tarjeta.
 class Tarjeta{
-    constructor(marca, numero, cardholder, vencimiento){
+    constructor(marca, numero, cardholder, dia, mes, año, importe) {
         this.marca = marca;
         this.numero = numero;
         this.cardholder = cardholder;
-        this.vencimiento = vencimiento;
+        this.dia= dia;
+        this.mes = mes;
+        this.año = año;
+        this.importe = importe;
     }
 }
 
 // Invocar un método que devuelva toda la información de una tarjeta.
 
 function mostrarTarjeta(){
-    let tarjeta = new Tarjeta("VISA", "123456789", "Juan", "12/20");
-    console.log(tarjeta); 
+    console.log("marca de la tarjeta: "+this.marca+" numero de tarjeta: "+this.numero+" nombre de usuario: "+this.cardholder+" vencimiento: "+this.dia+"/"+this.mes+"/"+this.año+" monto de importe: "+this.importe);
 }
 
 // Informar si una operación es válida.
-function validarOperacion(){
-    let tarjeta = new Tarjeta("VISA", "123456789", "Juan", "12/20");
-    if(tarjeta.marca == "VISA"){
-        console.log("Operacion valida");
-    }else{
-        console.log("Operacion invalida");   
+
+function validarOperacion(monto){
+    if(monto<1000){
+        console.log("operacion valida, monto a extraer: "+monto)
+    }
+    else{
+        console.log("operacion invalida, usted quiso extraer: "+monto)
     }
 }
 
 // Informar si una tarjeta es valida para operar.
+
 function validarTarjeta(){
     let tarjeta = new Tarjeta("VISA", "123456789", "Juan", "12/20");
     if(tarjeta.marca == "VISA" && tarjeta.vencimiento == "12/20"){
@@ -45,6 +49,7 @@ function validarTarjeta(){
 }
 
 // Obtener por medio de un método la tasa de operación informando marca e importe.
+
 function obtenerTasa(){
     let tarjeta = new Tarjeta("VISA", "123456789", "Juan", "12/20");
     if(tarjeta.marca == "VISA"){
