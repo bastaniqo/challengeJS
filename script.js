c61abe582f741a399b24e925228f91d7c60aaad9
Nicolas Tortora <nico.t@live.com.ar>
2022-07-29 21:30:04 -0300

    commit-final

--- script.js
+++ script.js
@@ -1,12 +1,33 @@
+
+var cards= []
+var send = document.getElementById('send')
+var numb = document.getElementById('cardNumb')
+var date = document.getElementById('date')
+var cardholder = document.getElementById('cardholder')
+var type = document.getElementById('marca')
+send.addEventListener('click', (event) => {
+    event.preventDefault();
+
+let card= {
+    marca:type.value,
+    numberOfCard:numb.value,
+    Name:cardholder.value,
+    date:date.value
+}
+cards.push(card)
+console.log(cards);
+
+
+});
+
 // Crear una clase ejecutable con 3 objetos de tipo tarjeta.
+
 class Tarjeta{
-    constructor(marca, numero, cardholder, dia, mes, año, importe) {
+    constructor(marca, numero, cardholder, vencimiento, importe){
         this.marca = marca;
         this.numero = numero;
         this.cardholder = cardholder;
-        this.dia= dia;
-        this.mes = mes;
-        this.año = año;
+        this.vencimiento = vencimiento;
         this.importe = importe;
     }
 }
@@ -17,28 +38,42 @@ function mostrarTarjeta(){
     console.log("marca de la tarjeta: "+this.marca+" numero de tarjeta: "+this.numero+" nombre de usuario: "+this.cardholder+" vencimiento: "+this.dia+"/"+this.mes+"/"+this.año+" monto de importe: "+this.importe);
 }
 
-// Informar si una operación es válida.
+// Informar si una operación es valida.
 
 function validarOperacion(monto){
     if(monto<1000){
-        console.log("operacion valida, monto a extraer: "+monto)
+        console.log("Operación válida, monto a extraer: "+monto);
     }
     else{
-        console.log("operacion invalida, usted quiso extraer: "+monto)
+        console.log("Operación inválida, usted quiso extraer: "+monto);
     }
 }
 
 // Informar si una tarjeta es valida para operar.
 
-function validarTarjeta(){
-    let tarjeta = new Tarjeta("VISA", "123456789", "Juan", "12/20");
-    if(tarjeta.marca == "VISA" && tarjeta.vencimiento == "12/20"){
-        console.log("Tarjeta valida");
-    }else{
-        console.log("Tarjeta invalida");
+function validateOperation(){
+    var date=new Date()
+    var fechaActual=date.getDate()
+    var mesActual=date.getMonth()
+    var añoActual=date.getFullYear()
+    if(this.dia>fechaActual){
+        if(this.mes>=mesActual){
+            if(this.año>=añoActual){
+                console.log("Su operación es valida, su vencimiento es superior a la fecha actual ("+fechaActual+"/"+mesActual+"/"+añoActual+")")
+        }
+        else{console.log("No se puede realizar la operación, su fecha de vencimiento caducó")}
     }
+    else{console.log("No se puede realizar la operación, su fecha de vencimiento caducó")}
+
 }
-// Indentificar si una tarjeta es distinta a otra.
+else if(this.dia<=fechaActual && this.mes>mesActual && this.año>=añoActual){
+    console.log("Su operación es válida, su vencimiento es superior a la fecha actual ("+fechaActual+"/"+mesActual+"/"+añoActual+")")
+}
+
+else{console.log("No se puede realizar la operación, su fecha de vencimiento caducó")}
+}
+
+// Identificar si una tarjeta es distinta a otra.
 
 function validarTarjeta(){
     let tarjeta1 = new Tarjeta("VISA", "123456789", "Juan", "12/20");
@@ -50,12 +85,24 @@ function validarTarjeta(){
 
 // Obtener por medio de un método la tasa de operación informando marca e importe.
 
-function obtenerTasa(){
-    let tarjeta = new Tarjeta("VISA", "123456789", "Juan", "12/20");
-    if(tarjeta.marca == "VISA"){
-        console.log("Tasa de operacion: 0.05");
-    }else if(tarjeta.marca == "NARA"){
-        console.log("Tasa de operacion: 0.03");
+function obtenerTasa() {
+    if(this.marca="visa"){
+        var tasa=this.año/this.mes
+        var precioFinal=this.importe-tasa
+        console.log("Tarjeta: "+this.marca+" monto de importe: "+this.importe+" tasa de la operacion: "+tasa+ " precio final incluyendo impuesto: "+precioFinal)
+
+    }
+    if(this.marca="nara"){
+        var tasa=this.mes*0.5
+        var precioFinal=this.importe-tasa
+        console.log("Tarjeta: "+this.marca+" monto de importe: "+this.importe+" tasa de la operacion: "+tasa+ " precio final incluyendo impuesto: "+precioFinal)
+    }
+    if(this.marca="amex"){
+        var tasa=this.mes*0.1
+        var precioFinal=this.importe-tasa
+        console.log("Tarjeta: "+this.marca+" monto de importe: "+this.importe+" tasa de la operacion: "+tasa+ " precio final incluyendo impuesto: "+precioFinal)
     }
 }
 
+
+//Los resultados se mostrarán en consola.
